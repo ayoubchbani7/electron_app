f5ed4f22569ebf20e7a886e9c889d2c089695d9a
achbani9 <a.chbani@eca-assurances.com>
2025-05-19 13:00:24 +0100

    AllProject

--- preload.js
+++ preload.js
@@ -1,3 +1,16 @@
-window.addEventListener('DOMContentLoaded',() => {
-    const replaceText = (selector,text)         
-});
+// Quand le contenu est chargé
+window.addEventListener('DOMContentLoaded', () => {
+  // Fonction qui permet d'injecter du texte
+  const replaceText = (selector, text) => {
+    const el = document.getElementById(selector)
+    if(el) {
+      el.innerHTML = text
+    }
+  }
+
+  // Boucle sur les types d'outils + appel fonction replaceText
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+  
+})
